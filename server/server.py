1f304588ad4403b38e46a4c726fa3be98a6d7c96
Christopher Frenning <christopher@frenning.com>
2023-07-05 19:08:48 +0000

    initial scaffolding

--- server/server.py
+++ server/server.py
@@ -28,6 +28,21 @@ def api():
                 "method": "GET",
                 "format": "json",
                 "help": "Returns a random matrix."
+            },
+            "get-token": {
+                "endpoint": "/api/get-token",
+                "params": [
+                    { "name": "fn", "desc": "original file name", "type": "string", "required": True }
+                ],
+                "method": "GET",
+                "format": "json",
+                "help": "Returns id and token to upload a new file."
+            },
+            "datasets": {
+                "endpoint": "/datasets",
+                "method": "GET",
+                "format": "json",
+                "help": "Returns id and token to upload a new file."
             }
         }
     }
@@ -43,28 +58,18 @@ def matrix():
 def home():
     return render_template('index.html')
 
-def getStorageAccountUrlAndKey():
+def get_storage_account_url_and_key():
     # Get the storage account name and key from the environment variables
     storageAccountName = os.environ.get('ZEPHYR_STORAGE_NAME')
     storageAccountKey = os.environ.get('ZEPHYR_STORAGE_KEY')
     return storageAccountName, storageAccountKey
-
-@app.route('/api/diagnostics', methods=['GET'])
-def diagnostics():
-    # Get the storage account name and key
-    storageAccountName, storageAccountKey = getStorageAccountUrlAndKey()
-
-    return jsonify({
-        'storageAccountName': storageAccountName,
-        'storageAccountKey': storageAccountKey
-    })
 
 class UserIdentity:
     def __init__(self, id, uname):
         self.id = id
         self.uname = uname
 
-def getUserIdentity():
+def get_user_identity():
     # TODO: Implement authentication, for now return a phony object with user id = 0
     return UserIdentity(id=0, uname='dataninja')
 
@@ -76,15 +81,25 @@ def insert_into_azure_table(storage_account_name, table_name, account_key, entit
     # Insert the entity
     table_client.create_entity(entity=entity)
 
-def getTableNameForDatasetRecords():
+def get_tablename_for_dataset_records():
     return 'datasets' # TODO: Parameterize
 
-def createDatasetMetadataRecord(blobId, sasToken, filename, userIdentity):
+def create_unique_metadata_id():
+    partition_key = shortuuid.random(length=3)
+    short_id = partition_key + '-' + shortuuid.random(length=7)
+    return partition_key, short_id
+
+def get_partition_key_from_metadata_id(metadata_id):
+    return metadata_id.split('-')[0]
+
+def create_dataset_metadata_record(blobId, sasToken, filename, userIdentity, metadata_id=None):
     # create an entry in azure table storage with the blobId, sasToken, filename, userIdentity and status='pending'
     # the table name is 'datasets'
     
-    partition_key = shortuuid.random(length=3)
-    short_id = partition_key + '-' + shortuuid.random(length=7)
+    partition_key, short_id = create_unique_metadata_id()
+    if metadata_id is not None:
+        partition_key = get_partition_key_from_metadata_id(metadata_id)
+        short_id = metadata_id
 
     entity = {
         'PartitionKey': partition_key,
@@ -93,14 +108,18 @@ def createDatasetMetadataRecord(blobId, sasToken, filename, userIdentity):
         'sasToken': sasToken,
         'filename': filename,
         'userId': userIdentity.id,
-        'status': 'pending'
+        'status': 'pending',
+        'name' : filename,
+        'desc' : 'A dataset',
+        'date' : datetime.utcnow().isoformat(),
+        'tags' : ''
     }
 
-    storage_account_name, account_key = getStorageAccountUrlAndKey()
-    insert_into_azure_table(storage_account_name, getTableNameForDatasetRecords(), account_key, entity)
+    storage_account_name, account_key = get_storage_account_url_and_key()
+    insert_into_azure_table(storage_account_name, get_tablename_for_dataset_records(), account_key, entity)
     return short_id
 
-def getDatasetDestinationUri(uid, id):
+def get_dataset_destination_uri(uid, id):
     return f'/u/{uid.uname}/{id}'
 
 @app.route('/api/get-token', methods=['GET'])
@@ -111,55 +130,87 @@ def token():
         abort(400, "Missing filename (fn query parameter)")
         
     # TODO: Map the unique id, user, and filename somewhere
-    uid = getUserIdentity()
+    uid = get_user_identity()
 
     # Get the storage account name and key
-    storageAccountName, storageAccountKey = getStorageAccountUrlAndKey()
+    storageAccountName, storageAccountKey = get_storage_account_url_and_key()
+
+    # Create metadata id
+    _, metadata_id = create_unique_metadata_id()
     
     # Create a SAS token for upload to a unique file name in the storage account
-    blob_id, uri = createUploadUriWithToken(storageAccountName, storageAccountKey)
+    blob_id, uri = create_upload_uri_with_token(storageAccountName, storageAccountKey, metadata_id)
     
     # Create a placeholder metadata record
-    metadata_id = createDatasetMetadataRecord(blobId=blob_id, sasToken=uri, filename=original_filename, userIdentity=uid)
+    metadata_id = create_dataset_metadata_record(blobId=blob_id, sasToken=uri, filename=original_filename, userIdentity=uid, metadata_id=metadata_id)
 
     return jsonify({
         'uri': uri,
-        #'id': id,
-        'dest': getDatasetDestinationUri(uid, id)
+        'id': metadata_id,
+        'dest': get_dataset_destination_uri(uid, metadata_id)
     })
 
-def getContainerNameForUploads():
+def get_container_name_for_uploads():
     return 'original-data' # TODO: Parameterize
 
-def createUploadUriWithToken(storageAccountName, storageAccountKey):
+def create_upload_uri_with_token(storage_account_name, storage_account_key, metadata_id):
     # Blobname
-    blobName = str(uuid.uuid4())
+    blob_id = str(uuid.uuid4())
+    blob_name = f'{metadata_id}/{blob_id}'
     # Create a SAS token that expires in an hour
-    tokenExpiry = datetime.utcnow() + timedelta(hours=1)
+    token_expiry = datetime.utcnow() + timedelta(hours=1)
     # Create a SAS token to upload a file to the storage account
-    sasToken = generate_blob_sas(
-        account_name=storageAccountName,
-        container_name=getContainerNameForUploads(),
-        blob_name=blobName,
-        account_key=storageAccountKey,
+    sas_token = generate_blob_sas(
+        account_name=storage_account_name,
+        container_name=get_container_name_for_uploads(),
+        blob_name=blob_name,
+        account_key=storage_account_key,
         permission=BlobSasPermissions(read=True, write=True),
-        expiry=tokenExpiry
+        expiry=token_expiry
         
     )
     # compose full url with token
-    return blobName, f'https://{storageAccountName}.blob.core.windows.net/{getContainerNameForUploads()}/{blobName}?{sasToken}'
+    return blob_name, f'https://{storage_account_name}.blob.core.windows.net/{get_container_name_for_uploads()}/{blob_name}?{sas_token}'
 
-def listAllDatasets():
-    account_name, account_key = getStorageAccountUrlAndKey()
+def create_download_uri_with_sas(storage_account_name, storage_account_key, blob_id):
+    blob_name = f'{blob_id}'
+    # Create a SAS token that expires in an hour
+    token_expiry = datetime.utcnow() + timedelta(hours=24)
+    sas_token = generate_blob_sas(
+        account_name=storage_account_name,
+        container_name=get_container_name_for_uploads(),
+        blob_name=blob_name,
+        account_key=storage_account_key,
+        permission=BlobSasPermissions(read=True),
+        expiry=token_expiry
+    )
+    # compose full url with token
+    return f'https://{storage_account_name}.blob.core.windows.net/{get_container_name_for_uploads()}/{blob_name}?{sas_token}'
+
+def get_username_from_userid(user_id):
+    return 'dataninja' # TODO: Implement
+
+def list_all_datasets():
+    account_name, account_key = get_storage_account_url_and_key()
     # get all records from the datasets table
-    table_name = getTableNameForDatasetRecords()
+    table_name = get_tablename_for_dataset_records()
     connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
     table_client = TableClient.from_connection_string(connection_string, table_name)
     entities = table_client.list_entities()
     
     # pick only the ones with status='complete'
     # return only RowKey, filename, userId
-    return [{'id':entity['RowKey'],'filename':entity['filename'],'userId':entity['userId'],'desc':'This is a static description of a dataset with points for each observation and statistical significance that can lead to conclusions without bias and visually interesting representations.'} for entity in entities if entity['status'] == 'pending']
+    return [ {
+        'id'        : entity['RowKey'],
+        'filename'  : entity['filename'],
+        'uid'       : entity['userId'],
+        'uname'     : get_username_from_userid(entity['userId']),
+        'name'      : entity['name'],
+        'desc'      : entity['desc'],
+        'date'      : entity['date'],
+        'tags'      : entity['tags'],
+        'blob_id'   : entity['blobId'],
+        } for entity in entities ]#if entity['status'] == 'pending']
 
 
 @app.route('/datasets', methods=['GET'])
@@ -168,17 +219,35 @@ def datasets():
     accept_header = request.headers.get('Accept')
     if accept_header is not None and accept_header == 'application/json':
         # Get all datasets from the datasets table
-        datasets = listAllDatasets()
+        datasets = list_all_datasets()
 
         # Return the datasets as json
         return jsonify(datasets)
     else:
         # Return the datasets as html
-        return render_template('datasets.html', datasets=listAllDatasets())
+        return render_template('datasets.html', datasets=list_all_datasets())
 
 @app.route('/datasets/top', methods=['GET'])
 def top_datasets():
     return datasets()
+
+@app.route('/datasets/<id>', methods=['GET'])
+def dataset(id):
+    datasets = list_all_datasets()
+    dataset = next((dataset for dataset in datasets if dataset['id'] == id), None)
+    if dataset is None:
+        abort(404, f"Dataset with id {id} not found")
+    print(dataset)
+    # if accept header is only json, return json
+    accept_header = request.headers.get('Accept')
+    if accept_header is not None and accept_header == 'application/json':
+        # Return the datasets as json
+        return jsonify(dataset)
+    else:
+        # Return the datasets as html
+        download_uri = create_download_uri_with_sas(*get_storage_account_url_and_key(), dataset['blob_id'])
+        print(download_uri)
+        return render_template('dataset.html', dataset=dataset, download_uri=download_uri)
 
 
 
--- worker/worker.py
+++ worker/worker.py
@@ -25,7 +25,7 @@ from azure.data.tables import TableClient
 def getIngestionQueueName():
     return 'ingestion' # TODO: Parameterize this
 
-def getStorageAccountUrlAndKey():
+def get_storage_account_url_and_key():
     # Get the storage account name and key from the environment variables
     storageAccountName = os.environ.get('ZEPHYR_STORAGE_NAME')
     storageAccountKey = os.environ.get('ZEPHYR_STORAGE_KEY')
@@ -43,15 +43,19 @@ def get_message_from_queue(account_name: str, account_key: str, queue_name: str)
         content.append( peeked_message.content )
     return content
 
-def subjectToBlobUri(subject):
+def subject_to_blob_uri(subject):
     # subject is in the form of /blobServices/default/containers/<container>/blobs/<blob>
     # we need to convert this to https://<storageaccount>.blob.core.windows.net/<container>/<blob>
-    account_name, _ = getStorageAccountUrlAndKey()
-    print(subject.split('/'))
-    return f'https://{account_name}.blob.core.windows.net/{subject.split("/")[4]}/{subject.split("/")[6]}'
+    account_name, _ = get_storage_account_url_and_key()
+    return f'https://{account_name}.blob.core.windows.net/{subject.split("/")[4]}/{subject.split("/")[6]}/{subject.split("/")[7]}'
 
-def processQueue():
-    account_name, account_key = getStorageAccountUrlAndKey()
+def subject_to_ids(subject):
+    blob_id = subject.split("/")[7]
+    metadata_id = subject.split("/")[6]
+    return blob_id, metadata_id
+
+def process_queue():
+    account_name, account_key = get_storage_account_url_and_key()
     # retrieve a message from the 'ingestion' queue
     # if there is a message, process it
     msg = get_message_from_queue(account_name, account_key, getIngestionQueueName())
@@ -60,32 +64,88 @@ def processQueue():
         data = json.loads(decoded_message)
         if data['eventType'] == 'Microsoft.Storage.BlobCreated':
             bloburi = data['subject']
-            print(f'BlobCreated: {subjectToBlobUri(bloburi)}')
-            processBlob(subjectToBlobUri(bloburi))
+            blob_id, metadata_id = subject_to_ids(bloburi)
+            blob_url = subject_to_blob_uri(bloburi)
+            process_blob(metadata_id, blob_id, blob_url)
 
-def download_blob(account_name: str, account_key: str, container_name: str, blob_name: str, download_path: str):
+def download_blob(account_name: str, account_key: str, container_name: str, metadata_id : str, blob_name: str, download_path: str):
     blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
-    blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+    blob_client = blob_service_client.get_blob_client(container_name, f'{metadata_id}/{blob_name}')
     # Download the blob to a file
     with open(download_path, "wb") as download_file:
         download_file.write(blob_client.download_blob().readall())
 
-def getContainerNameForUploads():
+def get_container_name_for_uploads():
     return 'original-data' # TODO: Parameterize
 
-def processBlob(bloburi):
+def insert_into_azure_table(storage_account_name, table_name, account_key, entity):
+    # Connect to the table client
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    table_client = TableClient.from_connection_string(connection_string, table_name)
+
+    # Insert the entity
+    table_client.create_entity(entity=entity)
+
+def update_azure_table(storage_account_name, table_name, account_key, entity):
+    # Connect to the table client
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    table_client = TableClient.from_connection_string(connection_string, table_name)
+
+    # Insert the entity
+    table_client.update_entity(mode='merge', entity=entity)
+
+def list_all_datasets():
+    account_name, account_key = get_storage_account_url_and_key()
+    # get all records from the datasets table
+    table_name = get_table_name_for_dataset_records()
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    table_client = TableClient.from_connection_string(connection_string, table_name)
+    entities = table_client.list_entities()
+    
+    # pick only the ones with status='complete'
+    # return only RowKey, filename, userId
+    return [{'id':entity['RowKey'],'filename':entity['filename'],'userId':entity['userId'],'desc':'This is a static description of a dataset with points for each observation and statistical significance that can lead to conclusions without bias and visually interesting representations.'} for entity in entities if entity['status'] == 'pending']
+
+def get_partition_key_from_id(id):
+    return id.split('-')[0]
+
+# find single record by partionkey and rowkey
+def find_dataset_by_id(id):
+    account_name, account_key = get_storage_account_url_and_key()
+    # get all records from the datasets table
+    table_name = get_table_name_for_dataset_records()
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    table_client = TableClient.from_connection_string(connection_string, table_name)
+    entity = table_client.get_entity(partition_key=get_partition_key_from_id(id), row_key=id)
+    return entity
+
+def get_table_name_for_dataset_records():
+    return 'datasets' # TODO: Parameterize
+
+def process_blob(metadata_id, blob_id, bloburi):
+    account_name, account_key = get_storage_account_url_and_key()
+    # get the metadata
+    metadata = find_dataset_by_id(metadata_id)
+    print(metadata)
+    # update the metadata to status=processing
+    metadata['status'] = 'processing'
+    update_azure_table(account_name, get_table_name_for_dataset_records(), account_key, metadata)
     # download the blob
-    account_name, account_key = getStorageAccountUrlAndKey()
-    container_name = getContainerNameForUploads()
-    blob_name = bloburi.split('/')[-1]
-    download_path = f'/tmp/{blob_name}'
-    download_blob(account_name, account_key, container_name, blob_name, download_path)
+    account_name, account_key = get_storage_account_url_and_key()
+    container_name = get_container_name_for_uploads()
+    download_path = f'/tmp/{blob_id}'
+    download_blob(account_name, account_key, container_name, metadata_id, blob_id, download_path)
+    
     # run the model
     # upload the results
+    # update the metadata to status=complete
+    #metadata['status'] = 'complete'
+    #update_azure_table(account_name, get_table_name_for_dataset_records(), account_key, metadata)
+    
     
 
 
 
 # main
 if __name__ == "__main__":
-    processQueue()
+    process_queue()
